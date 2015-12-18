7e432f4ac8fa3b9c0df9489cc9f606b3fc962225
Reidar Johansen <reijoh@users.noreply.github.com>
2015-12-18 11:15:24 +0100

    Update Check-RecommendedHotfixes.ps1

--- Check-RecommendedHotfixes.ps1
+++ Check-RecommendedHotfixes.ps1
@@ -2,22 +2,51 @@ param
 (
     [string[]]$ComputerName=$env:COMPUTERNAME
 )
-$UpdateNeeded = $False
-Function Check-HotfixInstalled
+<#
+Original Script by Reidar Johansen (@reidartwitt):
+https://github.com/reijoh/scomscripts/blob/master/Check-RecommendedHotfixes.ps1
+ 
+Adjusted based on modified version by Christopher Keyaert (christopher@vnext.be)
+Date: December 11th, 2015
+https://www.vnext.be/2015/12/11/scom-agentos-recommended-hotfix-kb/
+ 
+Based on the KB list available https://support.microsoft.com/en-us/kb/2843219
+Based on the KB list available http://blogs.technet.com/b/kevinholman/archive/2009/01/27/which-hotfixes-should-i-apply.aspx
+
+Modified again by Reidar Johansen (@reidartwitt)
+Date: December 17th, 2015
+#>
+# Global Variables
+$global:TotalUpdatesNeeded = 0
+$global:Description = $null
+$KB2003 = 'KB955360','KB981263','KB933061','KB981574','KB982168','KB982167','KB932370' # We do not check for KB968760 because it has been replaced by KB981574, and not KB980773 because it is uncluded in KB982168
+$KB2003NET4 = 'KB2484832'
+$KB2003IIS = 'KB960718'
+$KB2008 = 'KB968967','KB2553708','KB2710558','KB2458331','KB2812950','KB2622802','KB979458','KB981263' # We do not check for KB2495300 because KB2710558 is recommended instead, and not KB2506143 because this is the WMF 3.0 update and it is not compatible with applications like sharepoint 2010, exhcange 2010 so carefull consideration must apply
+$KB2008NET4 = 'KB2484832'
+$KB2008ClusSvc = 'KB968936'
+$KB2008DFSR = 'KB973275'
+$KB2008IIS = 'KB2163398'
+$KB2008R2 = 'KB2470949','KB2547244','KB2775511','KB2732673','KB2728738','KB2878378','KB2617858','KB2494158','KB2734909','KB2622802','KB2692929' # We do not check for KB2618982 on every server, only if IIS is installed
+$KB2008R2IIS = 'KB2618982'
+$KB2012 = 'KB2790831','KB2911101'
+$KB2012R2 = 'KB2911106','KB2919394','KB2955164' # We do not check for KB2923126 because it is included in update rollup KB2919394, and not KB2954185 because it is included in update rollup KB2955164
+Function Update-HotfixNeeded
 {
     param
     (
         [string]$HotfixID,
         [string]$ComputerName
     )
+    $HF = $null
     $HF = Get-HotFix -ComputerName $ComputerName -ErrorAction SilentlyContinue | where {$_.HotfixID -match $HotfixID}
     If(!($HF))
     {
-        Write-Output "$ComputerName need hotfix $HotfixID"
-        $UpdateNeeded = $True
+        $global:Description += "$ComputerName need hotfix $HotfixID`r`n"
+        $global:TotalUpdatesNeeded += 1
     }
 }
-Function Check-ServicePackInstalled
+Function Update-ServicePackNeeded
 {
     param
     (
@@ -29,8 +58,8 @@ Function Check-ServicePackInstalled
     $CSDVersion = Invoke-Command -ComputerName $ComputerName -ErrorAction SilentlyContinue -ScriptBlock {(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -ErrorAction SilentlyContinue | select @{N='CSDVersion'; E={$_.CSDVersion}}).CSDVersion}
     If($CSDBuildNumber -lt $BuildNumber -or $CSDVersion -lt $Version)
     {
-        Write-Output "$ComputerName need $Version"
-        $UpdateNeeded = $True
+        $global:Description += "$ComputerName need $Version`r`n"
+        $global:TotalUpdatesNeeded += 1
     }
 }
 foreach($Computer in $ComputerName)
@@ -67,50 +96,113 @@ foreach($Computer in $ComputerName)
     {
         '2003'
         {
-            Check-ServicePackInstalled -BuildNumber '5583' -Version 'Service Pack 2' -ComputerName $Computer
-            Check-HotfixInstalled -HotfixID 'KB955360' -ComputerName $Computer
+            Update-ServicePackNeeded -BuildNumber '5583' -Version 'Service Pack 2' -ComputerName $Computer
+            foreach($KB in $KB2003)
+            {
+                Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+            }
+            # Only check if .NET Framework 4.0 is installed
+            If(Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse|Get-ItemProperty -name Version -EA 0|where{ $_.PSChildName -match '^(?!S)\p{L}' -and $_.Version -match '^4.0'})
+            {
+                foreach($KB in $KB2003NET4)
+                {
+                    Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+                }
+            }
+            # Only check if IIS is installed
+            If(Get-Service -Name W3SVC -ComputerName $Computer -ErrorAction SilentlyContinue)
+            {
+                foreach($KB in $KB2003IIS)
+                {
+                    Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+                }
+            }
         }
         '2008'
         {
-            Check-ServicePackInstalled -BuildNumber '1621' -Version 'Service Pack 2' -ComputerName $Computer
-            Check-HotfixInstalled -HotfixID 'KB968967' -ComputerName $Computer
+            Update-ServicePackNeeded -BuildNumber '1621' -Version 'Service Pack 2' -ComputerName $Computer
+            foreach($KB in $KB2008)
+            {
+                Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+            }
+            # Only check if .NET Framework 4.0 is installed
+            If(Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse|Get-ItemProperty -name Version -EA 0|where{ $_.PSChildName -match '^(?!S)\p{L}' -and $_.Version -match '^4.0'})
+            {
+                foreach($KB in $KB2008NET4)
+                {
+                    Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+                }
+            }
+            # Only check if Failover Clustering feature is installed
+            If(Get-Service -Name ClusSvc -ComputerName $Computer -ErrorAction SilentlyContinue)
+            {
+                foreach($KB in $KB2008ClusSvc)
+                {
+                    Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+                }
+            }
+            # Only check if DFS Replication feature is installed
+            If(Get-Service -Name dfsr -ComputerName $Computer -ErrorAction SilentlyContinue)
+            {
+                foreach($KB in $KB2008DFSR)
+                {
+                    Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+                }
+            }
+            # Only check if IIS is installed
+            If(Get-Service -Name W3SVC -ComputerName $Computer -ErrorAction SilentlyContinue)
+            {
+                foreach($KB in $KB2008IIS)
+                {
+                    Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+                }
+            }
         }
         '2008R2'
         {
-            Check-ServicePackInstalled -BuildNumber '1130' -Version 'Service Pack 1' -ComputerName $Computer
-            Check-HotfixInstalled -HotfixID 'KB2470949' -ComputerName $Computer
-            Check-HotfixInstalled -HotfixID 'KB2547244' -ComputerName $Computer
-            # Only check if IIS is installed
-            If(Get-Service W3SVC -ComputerName $Computer -ErrorAction SilentlyContinue)
+            Update-ServicePackNeeded -BuildNumber '1130' -Version 'Service Pack 1' -ComputerName $Computer
+            foreach($KB in $KB2008R2)
             {
-                Check-HotfixInstalled -HotfixID 'KB2618982' -ComputerName $Computer
+                Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+            }
+            # Only check if IIS is installed
+            If(Get-Service -Name W3SVC -ComputerName $Computer -ErrorAction SilentlyContinue)
+            {
+                foreach($KB in $KB2008R2IIS)
+                {
+                    Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+                }
             }
         }
         '2012'
         {
-            Check-HotfixInstalled -HotfixID 'KB2790831' -ComputerName $Computer
+            foreach($KB in $KB2012)
+            {
+                Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
+            }
         }
         '2012R2'
         {
-            # Only check if DNS role is installed
-            $DNSStartNumber = Invoke-Command -ComputerName $Computer -ErrorAction SilentlyContinue -ScriptBlock {(Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\DNS' -ErrorAction SilentlyContinue | select @{N='Start'; E={$_.Start}}).Start}
-            If($DNSStartNumber -and $DNSStartNumber -ne 4)
+            foreach($KB in $KB2012R2)
             {
-                Check-HotfixInstalled -HotfixID 'KB2919394' -ComputerName $Computer
-            }
-            # Only check if Domain Controller role
-            If((Get-WMIObject -Class Win32_ComputerSystem -ComputerName $Computer -Namespace 'root\cimv2' -ErrorAction SilentlyContinue).DomainRole -ge 4)
-            {
-                Check-HotfixInstalled -HotfixID 'KB2955164' -ComputerName $Computer
+                Update-HotfixNeeded -HotfixID $KB -ComputerName $Computer
             }
         }
     }
 }
-If($UpdateNeeded)
+If($TotalUpdatesNeeded -gt 0)
 {
-    "Install hotfixes!
+    If($ComputerName.Count -eq 1)
+    {
+        "A total of $TotalUpdatesNeeded updates missing on $ComputerName"+':'
+    }
+    Else
+    {
+        "A total of $TotalUpdatesNeeded updates missing on $($ComputerName.Count) computers:"
+    }
+    $Description|sort
 }
 Else
 {
-    "All OK!"
+    'All OK!'
 }
